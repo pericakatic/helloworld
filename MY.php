ec31cc066039819e6cd143a902b90543ea57bebd
= <perica.katic@gmail.com>
2015-04-13 07:55:27 +0200

    Adding new method Tuesday

--- MY.php
+++ MY.php
@@ -3,8 +3,12 @@ Class Days {
 	public function Monday() {
 		echo 'I\m sleeping';
 	}
+
+	public function Tuesday() {
+		echo 'Wake up and start coding real stuff :)';
+	}
 }
 
 $days = new Days;
-$days->monday();
+$days->Tuesday();
 ?>
